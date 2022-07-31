74ea1efb20130c4904999d322560c61dab0da737
RomanStashuk <romanstashuk1@gmail.com>
2022-07-31 16:20:51 +0300

    init project

---
+++ gulpfile.babel.js/config/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const isProd = process.argv.includes('--production');
+const isDev = !isProd;
+
+export default {
+  isProd: isProd,
+  isDev: isDev,
+
+  htmlmin: {
+    collapseWhitespace: isProd
+  },
+
+  pug: {
+    pretty: isDev
+  },
+
+  webpack: {
+    mode: isProd ? 'production' : 'development'
+  },
+
+  fonter: {
+    formats: ['ttf', 'woff', 'eot', 'svg']
+  }
+};
